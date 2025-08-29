48cd6c7c7569df00bc218f60c366dae513cadd94
leandrolopez06 <leandrolopez.06@gmail.com>
2025-08-29 17:37:05 -0300

    Se realiza entrega de Trabajo Practico N 1

---
+++ HolaMundo.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Ejercicio 2
+        /*System.out.println("¡Hola, Java!");*/
+        
+        //Ejercicio 3
+        /*String nombre= "Leandro";
+        int edad = 28;
+        double altura = 1.89;
+        boolean estudiante = true;
+        
+        System.out.println("nombre: " + nombre + "\nedad: " + edad + "\nAltura: " + altura + "\nEstudiante: " + estudiante);*/
+        
+        //Ejercicio 4
+        /*Scanner input= new Scanner (System.in);
+        String nombre;
+        int edad;
+        
+        System.out.print("Ingrese su nombre: ");
+        nombre= input.nextLine();
+        
+        System.out.print("Ingrese su edad: ");
+        edad= input.nextInt();
+        
+        System.out.println("Su nombre es " + nombre +  " y tiene " + edad + " años");*/
+        
+        //Ejercicio 5
+        /*Scanner input= new Scanner (System.in);
+        int num1,num2,suma,resta,division,multiplicacion;
+        
+        System.out.print("Ingrese un numero: ");
+        num1= input.nextInt();
+        
+        System.out.print("Ingrese otro numero: ");
+        num2= input.nextInt();
+        
+        suma= num1 + num2;
+        resta= num1 - num2;
+        division= num1 / num2;
+        multiplicacion=num1 * num2;
+        
+        System.out.println("Suma: " + suma + "\nResta: " + resta + "\nDivision: " + division + "\nMultiplicacion: " + multiplicacion);*/
+        
+        
+        //Ejercicio 6
+        /*String nombre= "Juan Perez";
+        int edad=30;
+        String direccion="Calle Falsa 123";
+        
+        System.out.println("Nombre: " + nombre + "\nedad: " + edad + " años\nDireccion: " + direccion);*/
+        
+        //Ejercicio 8
+        /*Scanner input= new Scanner (System.in);
+        double num1,num2,division;
+        
+        System.out.print("Ingrese un numero: ");
+        num1= input.nextDouble();
+        
+        System.out.print("Ingrese otro numero: ");
+        num2= input.nextDouble();
+        
+        division= num1 / num2;
+        
+        System.out.println(division);*/
+        
+        //Ejercicio 10
+        /*int a = 5;
+        int b = 2;
+        int resultado = a / b;
+        System.out.println("Resultado: " + resultado);*/
+       
+                
+              
+        
+        
+        
+        
+        
+                
+        
+                
+        
+        
+        
+       
+        
+        
+       
+    }
+    
+}
